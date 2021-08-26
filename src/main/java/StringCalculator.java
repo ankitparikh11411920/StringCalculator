93432e8e4e015962ebb5359234253be82de896a7
Ankit Parikh <ankit.parikh11411920@gmail.com>
2021-08-26 18:17:27 +0530

    ninth step commit

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,48 +1,54 @@
 public class StringCalculator {
     static int calledCount = 0;
+
     public int add(String s) {
         calledCount++;
-        if(s.isEmpty()) {
+        if (s.isEmpty()) {
             return 0;
         }
-        if(s.startsWith("//")){
+        if (s.startsWith("//")) {
             char delimeter = s.charAt(2);
             String value = s.substring(4);
             String[] numbers = value.split(String.valueOf(delimeter));
             return sum(numbers);
         }
-        if(s.contains(",") ||  s.contains("\n")){
-            s = s.replaceAll("\n",",");
+        if (s.contains(",") || s.contains("\n")) {
+            s = s.replaceAll("\n", ",");
             String[] numbers = s.split(",");
             return sum(numbers);
 
         }
         return Integer.parseInt(s);
     }
-    int getCalledCount(){
+
+    int getCalledCount() {
         return calledCount;
     }
+
     int sum(String[] array) {
         int sum = 0;
         for (String number : array) {
-            try {
-                if (Integer.parseInt(number) < 0) {
-                    throw new InvalidNumberException(array);
-                } else {
-                    sum += Integer.parseInt(number);
+            if (Integer.parseInt(number) <= 1000) {
+                try {
+                    if (Integer.parseInt(number) < 0) {
+                        throw new InvalidNumberException(array);
+                    } else {
+                        sum += Integer.parseInt(number);
+                    }
+                } catch (InvalidNumberException exc) {
+                    return 0;
                 }
-            } catch (InvalidNumberException exc) {
-                return 0;
             }
         }
         return sum;
     }
 }
-class InvalidNumberException extends Exception{
-    InvalidNumberException(String[] numbers){
+
+class InvalidNumberException extends Exception {
+    InvalidNumberException(String[] numbers) {
         System.out.println("negatives not allowed");
-        for(String number : numbers){
-            if(Integer.parseInt(number) < 0){
+        for (String number : numbers) {
+            if (Integer.parseInt(number) < 0) {
                 System.out.println(number);
             }
         }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -12,7 +12,7 @@ public class StringCalculatorTest {
         assertEquals(3,calculator.add("1,2"));
         assertEquals(15,calculator.add("1,2,3,4,5"));
         assertEquals(55,calculator.add("1,2,3,4,5,6,7,8,9,10"));
-        assertEquals(7954,calculator.add("11,11,11,1,45,23,98,100,7654"));
+        assertEquals(300,calculator.add("11,11,11,1,45,23,98,100,7654"));
         assertEquals(6,calculator.add("1\n2,3"));
         assertEquals(55,calculator.add("1\n2\n3\n4,5,6\n7,8\n9,10"));
         assertEquals(3,calculator.add("//;\n1;2"));
@@ -21,5 +21,7 @@ public class StringCalculatorTest {
         assertEquals(0,calculator.add("1,2,3,-4,5"));
         assertEquals(0,calculator.add("-1,-2,-3,-4,-5"));
         assertEquals(14,calculator.getCalledCount());
+        assertEquals(100, calculator.add("//;\n1;2;3;4;5;85;1001"));
+
     }
 }
